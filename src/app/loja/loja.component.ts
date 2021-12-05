f6487f52ed50058354c98cd707c35628b0a51587
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-12-05 12:18:34 -0300

    Implementação dos botçoes para adicionar os produtos]

--- src/app/loja/loja.component.ts
+++ src/app/loja/loja.component.ts
@@ -7,4 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LojaComponent{
 
+   //produtos: Array<string> = [];
+   produtos:any = [];
+
+    AdicinarAbacate(): void{
+      //Adiciona uma banana ao componente
+      this.produtos.push(
+        {
+          'icon':'icon_abacate.png',
+          'nome':'Abacate'
+        }
+      );
+    }
+    
+    AdicinarAlface(): void{
+      //Adiciona uma banana ao componente
+      this.produtos.push(
+        {
+          'icon':'icon_alface.png',
+          'nome':'Alface'
+        }
+      );
+    }
+
+    AdicinarBananas(): void{
+      //Adiciona uma banana ao componente
+      this.produtos.push(
+        {
+          'icon':'icon_banana.png',
+          'nome':'Banana'
+        }
+      );
+    }
+
+    AdicinarCenoura(): void{
+      //Adiciona uma banana ao componente
+      this.produtos.push(
+        {
+          'icon':'icon_cenoura.png',
+          'nome':'Cenoura'
+        }
+      );
+    }
+
+    AdicinarMaca(): void{
+      //Adiciona uma banana ao componente
+      this.produtos.push(
+        {
+          'icon':'icon_maca.png',
+          'nome':'Maçã'
+        }
+      );
+    }
   }
