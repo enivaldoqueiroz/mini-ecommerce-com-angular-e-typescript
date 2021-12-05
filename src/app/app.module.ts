38765a4936422b4a07ab4a1cdacaffc5b6fd2be3
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-12-05 01:25:25 -0300

    Criação dos componentes loja e estoque

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LojaComponent } from './loja/loja.component';
+import { EstoqueComponent } from './estoque/estoque.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LojaComponent,
+    EstoqueComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/estoque/estoque.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-estoque',
+    templateUrl:  './estoque.componet.html',
+    styleUrls: ['./estoque.componet.css']
+})
+
+export class EstoqueComponent{
+
+}
